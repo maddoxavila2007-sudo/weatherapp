5d0ce2ad7537ccb35f1b666ba2bbd06d7ccd966b
maddoxavila2007-sudo <maddoxavila2007@gmail.com>
2025-11-22 18:59:31 -0600

    Climaa

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" :class="weatherClass">
+  <div id="app">
     <main>
       <div class="search-box">
         <input
@@ -11,7 +11,7 @@
         />
       </div>
 
-      <div v-if="weather.main" class="weather-wrap">
+      <div v-if="weather.main" class="weather-wrap" :class="getWeatherClass(Math.round(weather.main.temp))">
         <div class="location-box">
           <div class="location">
             {{ weather.name }}, {{ weather.sys.country }}
@@ -71,26 +71,43 @@ function dateBuilder(d) {
   return `${day} ${date} ${month} ${year}`
 }
 
-</script>
-
-<style>
-body
-  .app-container {
-    transition: background-color 0.5s ease;
-    .bg-hot {background-color: #ff8c00;}
-    .bg-very-hot { background-color: #ff4500;}
-    .bg-cold {background-color: #4682b4;}
-    .bg-default {background-color: #4a4a4a}
+function getWeatherClass(temp) {
+  if (temp < 10) return 'frío';
+  else if (temp >= 10 && temp < 20) return 'templado';
+  else if (temp >= 20 && temp < 30) return 'calor';
+  else return 'muy-caluroso';
 }
 
+function colorFromTemp(temp) {
+  if (temp < 10) return '#ADD8E6'; // Light Blue for cold
+  else if (temp >= 10 && temp < 20) return '#8BC34A'; // Green for mild
+  else if (temp >= 20 && temp < 30) return '#FFA07A'; // Light Salmon for warm
+  else return '#f53535'; // Red for hot
+  backgroundColor
+}
+  </script>
 
+
+
+<style>
+.frío {
+  background-color: #ADD8E6;}
+
+.templado {
+  background-color: #8BC34A;}
+
+.calor {
+  background-color: #FFA07A;}
+
+.muy-caluroso {
+  background-color: #f53535;}
 
 main {
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  transition: background-color 0.5s ease;
 }
 
 .search-box {
@@ -113,6 +130,14 @@ main {
 .weather-box .weather {
   font-size: 24px;
   margin-top: 10px;
+}
+.location-box .location {
+  font-size: 32px;
+  font-weight: bold;
+}
+.location-box .date {
+  font-size: 20px;
+  margin-top: 5px;
 }
 
 </style>
